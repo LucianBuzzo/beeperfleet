f24ff6ea44a07c73d3eb8826e2a96f970e7bb74f
Lucian Buzzo <lucian.buzzo@gmail.com>
2017-02-10 11:07:01 +0000

    Setting up a simple redux store

--- server/renderer.js
+++ server/renderer.js
@@ -1,6 +1,20 @@
 const { ipcRenderer, remote } = require('electron')
 const main = remote.require('./main.js')
+const Immutable = require('immutable')
+const redux = require('redux')
 
 ipcRenderer.on('clientConnect', (event, arg) => {
   console.log(arg)
 })
+
+const reducer = (state, action) => {
+  if (!state) {
+    state = Immutable.fromJS({
+      sequences: []
+    })
+  }
+
+  return state
+}
+
+let store = redux.createStore(reducer)
