9e1e8993a5db1657e7c485831b436d36f294ef11
Lucian Buzzo <lucian.buzzo@gmail.com>
2017-02-10 12:00:28 +0000

    Added support for react

--- client/renderer.jsx
+++ client/renderer.jsx
@@ -1,7 +1,12 @@
+'use babel'
+
 const socket = require('socket.io-client')('http://localhost:8080')
 const uuidV4 = require('uuid/v4')
-const SEQUENCE_LENGTH = 16
 const clientUUID = uuidV4()
+const React = require('react')
+const ReactDOM = require('react-dom')
+
+const SEQUENCE_LENGTH = 16
 
 const sequence = [...Array(SEQUENCE_LENGTH)].map(() => 0)
 
@@ -12,3 +17,8 @@ socket.on('connect', () => {
     sequence
   })
 })
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+)
