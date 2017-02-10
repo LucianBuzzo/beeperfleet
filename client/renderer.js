a0630cff3575f64a7ec62ec089608058bc9c5a8b
Lucian Buzzo <lucian.buzzo@gmail.com>
2017-02-10 10:35:51 +0000

    Setting up simple socket server

--- client/renderer.js
+++ client/renderer.js
@@ -1,3 +1,5 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+const socket = require('socket.io-client')('http://localhost:8080');
+
+socket.on('connect', () => {
+  console.log('connected');
+});
