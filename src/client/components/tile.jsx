192f9d03e596192ca48bc9e838d00aa149f52ce9
Lucian Buzzo <lucian.buzzo@gmail.com>
2017-02-17 14:12:37 +0000

    Update prop sliders to work on touch screens.
    This change makes the entire button a draggable control that will update
    whichever slider is visible.
    
    change-type: patch

--- src/client/components/tile.jsx
+++ src/client/components/tile.jsx
@@ -10,6 +10,11 @@ const {
   SEQ_PITCH_UPDATE,
 } = require('../services/actions')
 
+const dragIcon = document.createElement('img')
+dragIcon.src = './assets/blank.png'
+dragIcon.width = 0
+dragIcon.height = 0
+
 class Tile extends React.Component {
   componentWillMount() {
     socket.on('heartbeat', (step) => {
@@ -22,6 +27,8 @@ class Tile extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      mode: 'pitch',
+      lastPos: {},
       active: false,
       highlight: false,
       pitch: 60,
@@ -30,6 +37,13 @@ class Tile extends React.Component {
     }
 
     this.handleChange = this.handleChange.bind(this)
+    this.handleDrag = this.handleDrag.bind(this)
+    this.handleDragEnd = this.handleDragEnd.bind(this)
+
+    store.subscribe(() => {
+      let mode = store.getState().get('mode')
+      this.setState({ mode })
+    })
   }
 
   stopIt(e) {
@@ -76,15 +90,73 @@ class Tile extends React.Component {
     })
   }
 
+  handleDrag(e) {
+    const event = e.nativeEvent
+    if (!this.state.active) {
+      return
+    }
+    // check to make sure there is data to compare against
+    if (typeof this.state.lastPos.x !== 'undefined') {
+
+        // get the change from last position to this position
+        let deltaX = this.state.lastPos.x - event.clientX
+        let deltaY = this.state.lastPos.y - event.clientY
+        let update = false
+
+        // check which direction had the highest amplitude and then figure out direction by checking if the value is greater or less than zero
+        if (Math.abs(deltaX) > Math.abs(deltaY) && deltaX > 0) {
+            // left
+        } else if (Math.abs(deltaX) > Math.abs(deltaY) && deltaX < 0) {
+            // right
+        } else if (Math.abs(deltaY) > Math.abs(deltaX) && deltaY > 0) {
+            // up
+            update = true
+        } else if (Math.abs(deltaY) > Math.abs(deltaX) && deltaY < 0) {
+            // down
+            update = true
+        }
+
+        if (update) {
+          let slider = e.target.querySelector('.slider-' + this.state.mode)
+          let val = parseInt(slider.value, 10)
+          let ratio = (slider.max - slider.min) / 100
+          slider.value = val - deltaY * ratio * 2
+          this.handleChange({ target: slider }, slider.dataset.prop)
+        }
+    }
+
+    // set the new last position to the current for next time
+    this.setState({
+      lastPos: {
+        x: event.clientX,
+        y: event.clientY,
+      },
+    })
+  }
+
+  handleDragStart(e) {
+    e.nativeEvent.dataTransfer.effectAllowed = 'move'
+    e.nativeEvent.dataTransfer.setDragImage(dragIcon, 0, 0)
+  }
+
+  handleDragEnd() {
+    this.setState({ lastPost: {} })
+  }
+
   render() {
     return (
       <button
+        draggable="true"
+        onDrag={this.handleDrag}
+        onDragStart={this.handleDragStart}
+        onDragEnd={this.handleDragEnd}
         onTouchStart={(e) => this.handleChange(e, 'active')}
         onClick={(e) => this.handleChange(e, 'active')}
         className={"tile " + (this.state.highlight ? 'highlight' : '') + ' ' + (this.state.active ? 'active' : '')}>
         <input
           onClick={this.stopIt}
           onChange={(e) => this.handleChange(e, 'pitch')}
+          data-prop='pitch'
           className="control-slider slider-note"
           type="range"
           value={71 - this.state.pitch}
@@ -93,6 +165,7 @@ class Tile extends React.Component {
         <input
           onClick={this.stopIt}
           onChange={(e) => this.handleChange(e, 'gain')}
+          data-prop='gain'
           className="control-slider slider-volume"
           type="range"
           value={100 - this.state.gain * 100}
@@ -101,6 +174,7 @@ class Tile extends React.Component {
         <input
           onClick={this.stopIt}
           onChange={(e) => this.handleChange(e, 'length')}
+          data-prop='length'
           className="control-slider slider-length"
           type="range"
           min="0"
